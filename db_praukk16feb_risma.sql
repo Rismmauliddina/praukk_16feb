53fdde072c8c7dedc15fc21ff85ebea13058b072
Rismmauliddina <125439556+Rismmauliddina@users.noreply.github.com>
2023-02-16 11:13:55 +0700

    Add files via upload

---
+++ db_praukk16feb_risma.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Feb 2023 pada 05.13
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.3.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_praukk16feb_risma`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_bahan`
+--
+
+CREATE TABLE `tb_bahan` (
+  `id_bahan` int(10) NOT NULL,
+  `nama_bahan` varchar(30) NOT NULL,
+  `jenis_bahan` varchar(50) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `kondisi` enum('baik','rusak') NOT NULL,
+  `tgl_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_bahan`
+--
+
+INSERT INTO `tb_bahan` (`id_bahan`, `nama_bahan`, `jenis_bahan`, `stok`, `harga`, `kondisi`, `tgl_masuk`) VALUES
+(1, 'fghrfg', 'dfgdg', 9, 19000, 'baik', '2023-02-12'),
+(2, 'kertas', 'kayu', 10, 19, 'rusak', '2023-02-01'),
+(3, 'emas', 'logam', 23, 1200, 'rusak', '2023-02-12'),
+(4, 'emas', 'kayu', 20, 10, 'baik', '2023-02-01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(10) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `id_bahan` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nama_user`, `jabatan`, `id_bahan`) VALUES
+(2, 'billaa', 'admin togell', 2),
+(3, 'sholekah', 'admin', 3),
+(4, 'sholekah', 'admin', 4);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  ADD PRIMARY KEY (`id_bahan`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD KEY `user_bahan` (`id_bahan`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_bahan`
+--
+ALTER TABLE `tb_bahan`
+  MODIFY `id_bahan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD CONSTRAINT `user_bahan` FOREIGN KEY (`id_bahan`) REFERENCES `tb_bahan` (`id_bahan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
